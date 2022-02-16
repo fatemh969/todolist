c4960452463bfb38625d6384d8ea82b3068e3138
nafasmohammadi999 <nafasmohammadi999@gmail.com>
2022-02-16 15:54:56 +0330

    Updates

---
+++ precache-manifest.0ed9a223b6ec4f4eec59cf4367f638cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32dc8890887641ef1cb0b3c032a1257",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9f679a880f9932c0ba5d",
+    "url": "/todolist/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "9b29988a0770094e9977",
+    "url": "/todolist/static/css/main.44ac3138.chunk.css"
+  },
+  {
+    "revision": "9f679a880f9932c0ba5d",
+    "url": "/todolist/static/js/2.22c108c3.chunk.js"
+  },
+  {
+    "revision": "9b29988a0770094e9977",
+    "url": "/todolist/static/js/main.56872b85.chunk.js"
+  },
+  {
+    "revision": "481c345fea95ac730049",
+    "url": "/todolist/static/js/runtime-main.c570b5cc.js"
+  }
+]);
